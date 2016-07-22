27b88db56a99974290e3d90a6c165d6e5145c8ab
stevebrush <steve.brush@blackbaud.com>
2016-07-21 20:33:34 -0400

    Added edit buttons

--- public/app/views/admin/beacons/beacons.js
+++ public/app/views/admin/beacons/beacons.js
@@ -1,17 +1,28 @@
 (function (angular) {
     'use strict';
 
-    function BeaconsListController(BeaconService) 
-    {
+    function BeaconsListController($state, BeaconService) {
         var self = this,
             beacons;
 
         BeaconService.getAll().then(function(data) {
-            self.beacons = data.value;    
+            self.beacons = data.value;
         });
+
+        self.goToEdit = function (beacon) {
+            switch (beacon.beaconType) {
+            case 'docent':
+                $state.go('admin.forms.docent', { id: beacon._id });
+                break;
+            case 'exhibit':
+                $state.go('admin.forms.exhibit', { id: beacon._id });
+                break;
+            }
+        };
     }
 
     BeaconsListController.$inject = [
+        '$state',
         'BeaconService'
     ];
 
